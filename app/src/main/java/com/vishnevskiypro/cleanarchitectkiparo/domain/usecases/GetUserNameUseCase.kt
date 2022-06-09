7b5d0177ebdf8b11730cc311df6426207c34eb25
Oleg <vishnevskiyoleg@gmail.com>
2022-06-10 01:37:59 +0300

    Clean Architecture

--- app/src/main/java/com/vishnevskiypro/cleanarchitectkiparo/domain/usecases/GetUserNameUseCase.kt
+++ app/src/main/java/com/vishnevskiypro/cleanarchitectkiparo/domain/usecases/GetUserNameUseCase.kt
@@ -3,6 +3,7 @@ package com.vishnevskiypro.cleanarchitectkiparo.domain.usecases
 import com.vishnevskiypro.cleanarchitectkiparo.domain.models.UserName
 import com.vishnevskiypro.cleanarchitectkiparo.domain.repository.UserRepository
 
+
 class GetUserNameUseCase(private val userRepository: UserRepository) {
 
     fun execute(): UserName{
